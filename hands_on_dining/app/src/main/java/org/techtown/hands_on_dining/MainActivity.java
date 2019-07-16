5ebc2fa9f877b4662d9ef91eb1aec2d7e528f0f5
LeeJinho <kartmon@naver.com>
2019-07-16 18:41:39 +0900

    first upload

---
+++ hands_on_dining/app/src/main/java/org/techtown/hands_on_dining/MainActivity.java
@@ -0,0 +1,14 @@
+package org.techtown.hands_on_dining;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
